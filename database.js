fb4be88ce80728c2c93a27a10e63a1aae2baa4e1
varadarajus <varadarajusivala2218@gmail.com>
2025-10-04 19:51:21 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,6 +1,17 @@
-const sqlit3 = require('sqlite3').verbose();
-const db=new sqlit3.Database("/user.db");
+const path = require("path");
+const sqlite3 = require("sqlite3").verbose();
 
-db.run("create table if not exists user (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT UNIQUE, password TEXT)");
+// Save database in Render's writable tmp folder
+const dbPath = path.join(__dirname, "data", "users.db");
 
-module.exports=db;
+// Make sure the folder exists
+const fs = require("fs");
+const dir = path.dirname(dbPath);
+if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
+
+const db = new sqlite3.Database(dbPath, (err) => {
+  if (err) console.error("Error opening database", err);
+  else console.log("Database opened at", dbPath);
+});
+
+module.exports = db;
